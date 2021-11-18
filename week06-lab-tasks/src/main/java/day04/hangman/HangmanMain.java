bd4960b1f2c311e676b2cdb6eed38b897b8dcb89
anitaszoke <szanita1735@gmail.com>
2021-11-18 16:27:30 +0100

    Day04 konzultáció feladat Hangman

---
+++ week06-lab-tasks/src/main/java/day04/hangman/HangmanMain.java
@@ -0,0 +1,48 @@
+package day04.hangman;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class HangmanMain {
+    String wordToFind = "alma";
+    String wordFound = "____";
+    int chances = 8;
+
+    public static void main(String[] args) {
+        new HangmanMain().run();
+    }
+
+    private void run() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Itt tartunk: " + wordFound);
+            System.out.println("Ennyi tipped van még: " + chances);
+            System.out.println("Mi a tipped?");
+            String guess = scanner.nextLine();
+
+            if (wordToFind.contains(guess)) {
+                wordFound = updateWordFound(wordToFind, wordFound, guess);
+            } else {
+                chances--;
+            }
+
+        } while (!wordToFind.equals(wordFound) && (chances >= 1));
+
+        if (wordToFind.equals(wordFound)) {
+            System.out.println("Ügyes!");
+        } else {
+            System.out.println("Vesztettél!");
+        }
+    }
+
+    private String updateWordFound(String wordToFind, String status, String guess) {
+        char[] chars = status.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (wordToFind.charAt(i) == guess.charAt(0)) {
+                chars[i] = guess.charAt(0);
+            }
+        }
+        return new String(chars);
+    }
+}
+
